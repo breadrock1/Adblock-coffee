cb725ce93610fec380798fa9498960fc789a9d2f
breadrock <breadrock1@gmail.com>
2024-07-06 12:46:59 +0300

    chore(example): Updated test

--- examples/simple/src/test/java/com/example/simple/AppTest.java
+++ examples/simple/src/test/java/com/example/simple/AppTest.java
@@ -1,20 +1,37 @@
 package com.example.simple;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import com.example.adblock.AdvtBlocker;
+
 /**
- * Unit test for simple App.
+ * Simple test of using library
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void mainTest() {
+        List<String> rules = new ArrayList<>(List.of(
+            "-advertisement-icon.",
+            "-advertisement-management/",
+            "-advertisement.",
+            "-advertisement/script."
+        ));
+
+        AdvtBlocker blocker = AdvtBlocker.createInstance(rules);
+        boolean result = blocker.checkUrls(
+            "http://example.com/-advertisement-icon.",
+            "http://example.com/helloworld",
+            "image"
+        );
+
+        assertTrue(result);
     }
 }
